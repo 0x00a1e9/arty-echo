18ba24834f92a7db2ccb5962ab67ff58a3556395
cyan <cyan@0x00a1e9.dev>
2020-01-31 14:42:32 +0900

    Use a FIFO instead of reg for storing characters from RXD

--- rtl/uart_top.v
+++ rtl/uart_top.v
@@ -1,9 +1,9 @@
 module UART
 #(
     parameter [ 2:0] WAIT_ACTI      = 3'b001,
-    parameter [ 2:0] SEND_CHAR      = 3'b011,
-    parameter [ 2:0] RDY            = 3'b100,
-    parameter [ 2:0] WAIT_RDY       = 3'b101,
+    parameter [ 2:0] SEND_CHAR      = 3'b010,
+    parameter [ 2:0] RDY            = 3'b011,
+    parameter [ 2:0] WAIT_RDY       = 3'b100,
     parameter [ 3:0] BTN_STR_LEN    = 4'd8,
     parameter [ 1:0] PARROT_STR_LEN = 2'd3,
     parameter        BTN            = 1'b0,
@@ -16,12 +16,11 @@ module UART
     input  wire UART_RX    // A9
 );
 
-reg  [ 2:0] demo_state;
+reg  [ 2:0] uart_state;
 reg  [ 7:0] btn_str        [ 7:0];
 reg  [ 3:0] str_idx;
+reg         send_mode;
 reg         send_char;
-reg         btn_or_parrot;
-reg  [ 7:0] char_to_parrot;
 
 // UART_TX_CTRL
 wire        send;
@@ -32,10 +31,17 @@ assign send = send_char;
 
 // UART_RX_CTRL
 wire [ 7:0] recv_data;
-wire vald_data;
+wire        vald_data;
+
+// FIFO
+wire        w_enable;
+wire        r_enable;
+wire [ 7:0] r_data;
+wire [ 8:0] count;
+
 
 initial begin : asynchronous_reset
-    demo_state     <= WAIT_ACTI;
+    uart_state     <= WAIT_ACTI;
 
     btn_str[0]     <= 8'h02; // STX
     btn_str[1]     <= 8'h41; // A
@@ -45,8 +51,6 @@ initial begin : asynchronous_reset
     btn_str[5]     <= 8'h0D; // CR
     btn_str[6]     <= 8'h0A; // LF
     btn_str[7]     <= 8'h03; // ETX
-
-    char_to_parrot <= 8'd0;
 
     send_char      <= 1'b0;
     send_data      <= 0;
@@ -67,64 +71,70 @@ UART_RX_CTRL uart_rx_ctrl (
     vald_data
 );
 
+assign w_enable = (vald_data == 1'b1 && count < 2**8);
+assign r_enable = (count > 0 && uart_state == WAIT_ACTI)? 1 : 0;
+
+FIFO fifo (
+    CLK,
+    w_enable,
+    r_enable,
+    recv_data, // w_data
+    r_data,
+    count
+);
+
 always @(posedge CLK) begin : state_transition
-    case (demo_state)
+    case (uart_state)
         WAIT_ACTI:
-            if (btn_0 == 1'b1 ^ vald_data == 1'b1) begin
-                demo_state <= SEND_CHAR;
-                if (btn_0 == 1'b1)
-                    btn_or_parrot <= BTN;
-                else if (vald_data == 1'b1)
-                    btn_or_parrot <= PARROT;
+            if (r_enable) begin
+                uart_state <= SEND_CHAR;
+                send_mode  <= PARROT;
+            end
+            else if (btn_0 == 1'b1) begin
+                uart_state <= SEND_CHAR;
+                send_mode  <= BTN;
             end
         SEND_CHAR:
-            demo_state <= RDY;
+            uart_state <= RDY;
         RDY:
-            demo_state <= WAIT_RDY;
+            uart_state <= WAIT_RDY;
         WAIT_RDY:
             if (ready == 1'b1) begin
-                case (btn_or_parrot)
+                case (send_mode)
+                    PARROT: begin
+                        uart_state <= WAIT_ACTI;
+                    end
                     BTN: begin
                         if (str_idx == BTN_STR_LEN)
-                            demo_state <= WAIT_ACTI;
+                            uart_state <= WAIT_ACTI;
                         else
-                            demo_state <= SEND_CHAR;
+                            uart_state <= SEND_CHAR;
                     end
-                    PARROT:
-                        demo_state <= WAIT_ACTI;
                 endcase
             end
         default: // should never be reached
-            demo_state <= WAIT_ACTI;
+            uart_state <= WAIT_ACTI;
     endcase
 end
 
 always @(posedge CLK) begin : inc_idx
-    if (demo_state == WAIT_ACTI)
+    if (uart_state == WAIT_ACTI)
         str_idx <= 0;
-    else if (demo_state == SEND_CHAR)
+    else if (uart_state == SEND_CHAR)
         str_idx <= str_idx + 1;
 end
 
 always @(posedge CLK) begin : char_load
-    if (demo_state == SEND_CHAR) begin
+    if (uart_state == SEND_CHAR) begin
         send_char <= 1'b1;
-        case (btn_or_parrot)
-            BTN:
-                send_data <= btn_str[str_idx];
-            PARROT:
-                send_data <= char_to_parrot;
-        endcase
+        if (send_mode == PARROT)
+            send_data <= r_data;
+        else if (send_mode == BTN)
+            send_data <= btn_str[str_idx];
     end
     else begin
         send_char <= 1'b0;
     end
-end
-
-// echo back
-always @(posedge CLK) begin : parrot_recv_char
-    if (vald_data == 1'b1)
-        char_to_parrot <= recv_data;
 end
 
 endmodule
